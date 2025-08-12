5605d4d7ce2834b8d201e6cd11c7673fe78b7385
BytesPlatform <developer@bytesplatform.com>
2025-08-12 22:50:36 +0500

    sales removed by aliyan

--- src/modules/sales/units/units.module.ts
+++ src/modules/sales/units/units.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { UnitsController } from './units.controller';
 import { UnitsService } from './units.service';
-import { PrismaService } from '../../../../prisma/prisma.service';
+import { UnitsController } from './units.controller';
 
 @Module({
   controllers: [UnitsController],
-  providers: [UnitsService, PrismaService],
-  exports: [UnitsService]
+  providers: [UnitsService],
 })
 export class UnitsModule {}
