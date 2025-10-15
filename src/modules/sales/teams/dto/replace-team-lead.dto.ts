93c3d5b53d6f9340371b34f28a1c37e12b46dc57
maazkashif1 <maazk101103@gmail.com>
2025-10-16 02:22:07 +0500

    added swagger documentation for teams module in sales

--- src/modules/sales/teams/dto/replace-team-lead.dto.ts
+++ src/modules/sales/teams/dto/replace-team-lead.dto.ts
@@ -1,6 +1,11 @@
 import { IsNumber, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class ReplaceTeamLeadDto {
+  @ApiProperty({
+    description: 'ID of the new team lead to replace the current one',
+    example: 12,
+  })
   @IsNumber()
   @IsNotEmpty()
   newTeamLeadId: number;
